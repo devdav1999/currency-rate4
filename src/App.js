f49bb9cbd589df1bbbdc3165b1280a15c42cae37
Jensen David <feudavid92@gmail.com>
2020-05-30 17:23:33 -0700

    copied hw3 and preliminary components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+function App() {
+  const [mainCurrencies, setMainCurrencies] = useState(["HKD", "USD", "AUD", "GBP", "CAD"]);
+  const [rates, setRates] = useState([]);
+  
+
+  function doFetch() {
+    const url = "https://api.exchangeratesapi.io/latest";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("the data", data);
+            console.log(data.rates);
+            setRates(Object.entries(data.rates));
+            setMainCurrencies(mainCurrencies);
+        });
   }
+
+  function addCurrency(event){
+    const currency = event.target.textContent;
+    setMainCurrencies([...mainCurrencies, currency])
+
+  }
+  
+
+  function removeCurrency(event){
+    const currency = event.target.textContent;
+    setMainCurrencies(mainCurrencies.filter(cur => cur !== currency));
+  }
+
+  useEffect(doFetch, []);
+
+  return (
+    <div>
+      <div className='TopNav'>
+      <h1>Currency Rates Today</h1>
+      <h5>Please click on bar to find out current rate for Euro</h5>
+      <div className='currencyNav'>
+      {
+        rates
+        .map(rate =>(
+          mainCurrencies.includes(rate[0]) ?
+            <button className='countrySelection' onClick={removeCurrency}>
+            {rate[0]}
+            </button>
+          :
+            <button className='countrySelection' onClick={addCurrency}>
+            {rate[0]}
+            </button>
+        ))
+        }
+      </div>
+      </div>
+      
+      
+      <div className="BarContainer">
+      {
+        rates
+        .filter(rate => mainCurrencies.includes(rate[0]))
+        .map(rate =>(
+            <div className='Bar' style={{height: (1/rate[1] * 100) + '%'}}>
+            {rate[0]}
+            <br></br>
+            {rate[1]}
+            </div>
+        ))
+        }
+      </div>
+   </div>
+   );
 }
+
 
 export default App;
---
+++ src/components/BarContainer/BarContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function BarContainer() {
+  return (
+    
+  );
+}
+
+export default BarContainer;
